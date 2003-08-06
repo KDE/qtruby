f40e1e40bdceccbab194aff3af46664480a623c7
Alexander Kellett <lypanov@kde.org>
2003-08-06 12:51:26 +0000

    a new example (canvastest.rb) a split out of helper libs into base/ minor fixes in textedit.rb (including remove qpopupmenu hack)
    svn path=/trunk/kdebindings/qtruby/; revision=241262

--- qtruby/rubylib/examples/textedit/textedit.rb
+++ qtruby/rubylib/examples/textedit/textedit.rb
@@ -1,75 +1,9 @@
 #!/usr/bin/ruby -w
 
-require 'rexml/document'
 require 'Qt'
+require 'rexml/document'
 
-# # ambiguity problems with 
-# Qt::MessageBox.question(self, "File Already Exists", "Are you sure you want to overwrite this file?")
-# # and with popupmenu.insertItem(txt, reciever, slot):
-# # evil workaround
-class Qt::PopupMenu
-   def insertItem(txt, rec, slot)
-      id = super(txt)
-      self.connectItem(id, rec, slot)
-      id
-   end
-end
-
-class KIconCollection
-   IconInfo = Struct.new(:collection, :id, :filetype)
-   def initialize(icon_collections)
-      @icon_info = {}
-      icon_collections.each_pair {
-         |collection_name, collection|
-         collection.each_pair {
-            |key, value|
-            info = IconInfo.new(collection_name, value, "png")
-            @icon_info[key] = info
-         }
-      }
-   end
-   def dims
-      "16x16"
-   end
-   def kdedir
-      ENV["KDEDIR"]
-   end
-   def get_icon_path(icon_type)
-      info = @icon_info[icon_type]
-      "#{kdedir}/share/icons/default.kde/#{dims}/#{info.collection}/#{info.id}.#{info.filetype}"
-   end
-   def get_icon_set(icon_type)
-      path = get_icon_path(icon_type)
-      pixmap = Qt::Pixmap.new(path)
-      icon_set = Qt::IconSet.new
-      icon_set.setPixmap(pixmap, Qt::IconSet.Small)
-      icon_set
-   end
-   def make_qt_action(parent, text_with_accel, icon_type)
-      act = Qt::Action.new(parent)
-      act.setIconSet(get_icon_set(icon_type))
-      act.setMenuText(text_with_accel)
-      act
-   end
-
-end
-
-module Icons
-   FILE_NEW, FILE_OPEN, FILE_CLOSE, FILE_SAVE, FILE_SAVE_AS, EXIT = 1,2,3,4,5, 6
-end
-
-icon_collections = {
-   "actions" => {
-      Icons::FILE_NEW       => "filenew",
-      Icons::FILE_OPEN      => "fileopen",
-      Icons::FILE_CLOSE     => "fileclose",
-      Icons::FILE_SAVE      => "filesave",
-      Icons::FILE_SAVE_AS   => "filesaveas",
-      Icons::EXIT           => "exit"
-   }
-}
-$kIcons = KIconCollection.new(icon_collections)
-print "Using KDEDIR == ", $kIcons.kdedir, "\n"
+require '../base/rui.rb'
 
 class MyTextEditor < Qt::TextEdit
    signals 'saved()'
@@ -78,7 +12,7 @@ class MyTextEditor < Qt::TextEdit
       @images = {}
       @@next_image_id = 0
       super(w)
-      self.setTextFormat(Qt.RichText)
+      self.setTextFormat(Qt::RichText)
    end
    def insert_richtext(richtext)
       # todo, use a rand string
@@ -92,8 +26,8 @@ class MyTextEditor < Qt::TextEdit
    end
    def load_image(fname, image_id)
       pixmap = Qt::Pixmap.new(fname)
-      msfactory = Qt::MimeSourceFactory.defaultFactory()
-      msfactory.setPixmap(image_id, pixmap);
+      msfactory = Qt::MimeSourceFactory.defaultFactory
+      msfactory.setPixmap(image_id, pixmap)
       @images[image_id] = fname
       image_id
    end
@@ -147,7 +81,7 @@ class MyTextEditor < Qt::TextEdit
    def open
       fname = Qt::FileDialog.getOpenFileName
       return if fname.nil?
-      if not File.exists?(fname)
+      unless File.exists?(fname)
          Qt::MessageBox.critical(self, "File Does Not Exist", "Sorry, unable to find the requested file!")
          return
       end
@@ -159,7 +93,8 @@ class MyTextEditor < Qt::TextEdit
    end
    def save_as
       fname = Qt::FileDialog.getSaveFileName
-      if fname.nil? || File.exists?(fname)
+      return if fname.nil?
+      if File.exists?(fname)
          Qt::MessageBox.critical(self, "File Already Exists", "Sorry, file already exists. Please choose a non-existing filename!")
          return save_as
       end
@@ -170,9 +105,6 @@ class MyTextEditor < Qt::TextEdit
       emit saved()
    end
 end
-
-RAction = Struct.new(:text_with_accel, :icon_type, :rec, :slot, :included_in, :action)
-RSeperator = Struct.new(:included_in, :id)
 
 class MyWidget < Qt::MainWindow
    slots 'text_changed()', 'saved()'
@@ -193,20 +125,7 @@ class MyWidget < Qt::MainWindow
          RAction.new("E&xit", Icons::EXIT, $qApp, SLOT('quit()'), [fileMenu])
       ]
 
-      actions.each { |a|
-         if a.is_a? RSeperator
-            a.included_in.each {
-               |to| a.id = to.insertSeparator() 
-            }
-         else
-            qt_action = $kIcons.make_qt_action(self, a.text_with_accel, a.icon_type)
-            connect(qt_action, SIGNAL('activated()'), a.rec, a.slot)
-            a.included_in.each {
-               |to| qt_action.addTo(to)
-            }
-            a.action = qt_action
-         end
-      }
+      build_actions(actions)
 
       menubar = Qt::MenuBar.new(self)
       menubar.insertItem("&File", fileMenu)
