a80ae1077925bb6b8188595bc8af972e02c07ab9
Alexander Kellett <lypanov@kde.org>
2003-08-09 16:50:10 +0000

    a crash case from blarf
    svn path=/trunk/kdebindings/qtruby/; revision=241732

--- qtruby/rubylib/examples/testcases/bugs.rb
+++ qtruby/rubylib/examples/testcases/bugs.rb
@@ -1,8 +1,24 @@
+require 'Qt'
+
 def bug1
    p1 = Qt::Point.new(5,5)
-   p3 = p1.dup
-   p3.setX 5
+   p1.setX 5 
    p p1
+   p3 = p1.dup
+   p3.setX 5 
    p p3
 end
-#bug1
+class Foo < Qt::PushButton
+   def initialize
+      super
+   end
+end
+def bug2
+   a = Qt::Application.new(ARGV)
+   hello = Foo.new
+   hello.resize(100, 30)
+   a.setMainWidget(hello)
+   hello.show()
+   a.exec()
+end
+bug2
