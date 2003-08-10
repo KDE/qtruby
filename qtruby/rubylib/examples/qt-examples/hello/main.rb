e4b6660c5e79d9416a9979cb01a21c55bc60fe22
Alex Zepeda <zipzippy@sonic.net>
2003-08-10 09:24:45 +0000

    Hello, world example.
    svn path=/trunk/kdebindings/qtruby/; revision=241827

---
+++ qtruby/rubylib/examples/qt-examples/hello/main.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'Qt'
+require 'hello'
+
+a = Qt::Application.new(ARGV)
+s = ''
+
+ARGV[1..ARGV.size-1].each{|arg|
+	s += ' ' + arg
+}
+
+if (s.empty?)
+	s = 'Hello, World'
+end
+
+h = Hello.new(s)
+h.setCaption('QtRuby says hello')
+h.connect(h, SIGNAL('clicked()'), a, SLOT('quit()'))
+h.setFont(Qt::Font.new('times', 32, Qt::Font.Bold))	# default font
+h.setBackgroundColor(Qt::white)				# default bg color
+a.setMainWidget(h)
+h.show
+
+a.exec
