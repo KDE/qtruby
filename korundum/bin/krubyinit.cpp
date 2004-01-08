23c620cc894f4a71c8f9bc2bfaaa58f9f3a5a9fc
Alexander Kellett <lypanov@kde.org>
2004-01-08 09:57:12 +0000

    add a copyright, even though the code is probably in the public domain anyways...
    svn path=/trunk/kdebindings/korundum/; revision=277791

--- korundum/bin/krubyinit.cpp
+++ korundum/bin/krubyinit.cpp
@@ -1,4 +1,21 @@
-#include "ruby.h"
+#include <ruby.h>
+
+/***************************************************************************
+       krubyinit  -  makes use of kdeinit_wrapper possible for ruby programs
+                             -------------------
+    begin                : Wed Jan 7 2004
+    copyright            : (C) 2004 by Alexander Kellett
+    email                : lypanov@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 int main(int argc, char **argv) {
      ruby_init();
