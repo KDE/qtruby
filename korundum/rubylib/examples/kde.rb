d9b5f2a8ecac0d354b1b70307e09c729b7ec6c9e
Alexander Kellett <lypanov@kde.org>
2004-01-09 19:22:38 +0000

    hacky start of a webbrowser, because of lack of KURL i got the ruby to download the html instead..
    svn path=/trunk/kdebindings/korundum/; revision=278238

--- korundum/rubylib/examples/kde.rb
+++ korundum/rubylib/examples/kde.rb
@@ -2,29 +2,58 @@
 
 require 'Korundum'
 
-class MyBase < Qt::Object
-   slots "blah()"
-   def initialize mybutton
-      super
-      @knop  = mybutton
-      @count = 0
+
+
+class MyBase < Qt::HBox
+   slots "back()", "forward()"
+   attr_accessor :back, :forward, :url
+   def initialize *k
+      super *k
+      @url  = "http://www.kde.org/"
+      @forward = KDE::PushButton.new(self) { setText "Hello World" }
+      @back    = KDE::PushButton.new(self) { setText "Hello World" }
+      @label   = Qt::Label.new(self)
+      Qt::Object.connect( @forward, SIGNAL( "clicked()" ),
+                          self,     SLOT(   "back()" ) )
+      Qt::Object.connect( @back,    SIGNAL( "clicked()" ),
+                          self,     SLOT(   "forward()" ) )
    end
-   def blah
-      @knop.setText "#{@count+=1}"
+   def back
+      @label.setText "would go backwards"
+   end
+   def forward
+      @label.setText "would go forwards"
    end
 end
 
 about = KDE::AboutData.new("one", "two", "three")
 KDE::CmdLineArgs.init(1, ["four"], about)
 a = KDE::Application.new()
-hello = KDE::PushButton.new(nil) { setText "Hello World" }
-blah = MyBase.new hello
-Qt::Object.connect( hello, SIGNAL( "clicked()" ),
-                    blah,  SLOT( "blah()" ) )
 
-=begin
+browser = Qt::VBox.new
+
+blah = MyBase.new browser
+browser.show
+
+require 'net/http'
+require 'uri'
+
+output = Net::HTTP.get URI.parse(blah.url)
+p output
+w = KDE::HTMLPart.new browser
+w.begin
+w.write output
+w.end
+w.show
+
+a.setMainWidget(browser)
+a.exec()
+
+
+__END__
+
 b0rked due to KDE::URL non existance...
---
+
 url = KDE::URL.new "http://www.kde.org"
 p url.site
 w = KDE::HTMLPart.new
@@ -33,14 +62,11 @@ w.view.resize 500, 400
 w.show
 =end
 
-w = KDE::HTMLPart.new
-# TESTCASE - w = KDE::HTMLPart; then w.begin; crashes badly
+TESTCASE - 
+w = KDE::HTMLPart  # notice the missing .new
 w.begin
-w.write "blah <b>blah</b> blah "
-w.end
-w.show()
+=> crashes badly
 
-a.setMainWidget(w)
+dum di dum
+
 dcop = KDE::DCOPObject.new()
-w.show()
-a.exec()
