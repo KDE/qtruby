b9dac67542f01dcb98954d116ed8ca047543abd1
Alexander Kellett <lypanov@kde.org>
2004-08-01 17:15:51 +0000

    importing qrubyinit.cpp a perfect clone of krubyinit but with kde in place of qt prevents startup crashes for programs that don't wish to depend on kde
    svn path=/trunk/kdebindings/qtruby/; revision=334845

---
+++ qtruby/bin/qrubyinit.cpp
@@ -0,0 +1,29 @@
+#include <ruby.h>
+
+/***************************************************************************
+qrubyinit  -  makes use of kdeinit_wrapper possible for ruby programs
+-------------------
+    begin                : Wed Jan 7 2004
+    copyright            : (C) 2004 by Alexander Kellett
+    email                : lypanov@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// this name can be used to allow apps 
+// to detect what they were started with
+static const char* script_name = "qrubyinit_app";
+
+int main(int argc, char **argv) {
+     ruby_init();
+     ruby_script((char*)script_name);
+     ruby_options(argc, argv); 
+     ruby_run();
+}
