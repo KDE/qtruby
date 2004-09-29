7796239d106c6e9bcefe6ea9793d418b28d86c81
Richard Dale <richard.j.dale@gmail.com>
2004-09-29 17:45:26 +0000

    * Added Jim Menard's ruboids as an OpenGL/Qt::GL* Widgets example * Improved instructions and exconf.rb for building the gui extension from Michal 'hramrach' Suchanek
    svn path=/trunk/kdebindings/qtruby/; revision=350325

--- qtruby/rubylib/designer/uilib/extconf.rb
+++ qtruby/rubylib/designer/uilib/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
-dir_config('qtruby')
 dir_config('smoke')
 dir_config('qt')
-$LOCAL_LIBS += '-lqt-mt -lqui -lstdc++'
+$CPPFLAGS += " -I../../../../smoke -I../../qtruby "
+$LOCAL_LIBS += '-bundle_loader ../../qtruby/qtruby.bundle -lsmokeqt -lqui -lqt-mt -lstdc++'
 create_makefile("qui")
