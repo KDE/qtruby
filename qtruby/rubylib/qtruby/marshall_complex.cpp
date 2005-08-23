3c300e22e349ab3f2333b1b9137892a79c10eac4
Caleb Tennis <caleb@gentoo.org>
2005-08-23 18:03:56 +0000

    Add in a file
    svn path=/trunk/KDE/kdebindings/qtruby/; revision=452555

---
+++ qtruby/rubylib/qtruby/marshall_complex.cpp
@@ -0,0 +1,41 @@
+template <>
+static void marshall_from_ruby<long long>(Marshall *m) 
+{
+	VALUE obj = *(m->var());
+	m->item().s_voidp = new long long;
+	*(long long *)m->item().s_voidp = ruby_to_primitive<long long>(obj);
+	
+	m->next();
+	
+	if(m->cleanup() && m->type().isConst()) {
+		delete (long long int *) m->item().s_voidp;
+	}	
+}
+
+template <>
+static void marshall_to_ruby<long long>(Marshall *m)
+{
+	*(m->var()) = rb_ll2inum(*(long long *) m->item().s_voidp);
+}
+
+
+template <>
+static void marshall_from_ruby<unsigned long long>(Marshall *m) 
+{
+	VALUE obj = *(m->var());
+	m->item().s_voidp = new unsigned long long;
+	*(long long *)m->item().s_voidp = ruby_to_primitive<unsigned long long>(obj);
+
+	m->next();
+	
+	if(m->cleanup() && m->type().isConst()) {
+		delete (long long int *) m->item().s_voidp;
+	}	
+}
+
+
+template <>
+static void marshall_to_ruby<unsigned long long>(Marshall *m)
+{
+	*(m->var()) = rb_ull2inum(*(unsigned long long *) m->item().s_voidp);
+}
