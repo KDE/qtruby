773903c3b434d7c4ec346f61efef67f233b2ccb7
Richard Dale <richard.j.dale@gmail.com>
2006-04-10 12:34:11 +0000

    * Tidier code with blocks passed to constructors
    svn path=/trunk/KDE/kdebindings/qtruby/; revision=528213

--- qtruby/rubylib/examples/dialogs/configdialog/configdialog.rb
+++ qtruby/rubylib/examples/dialogs/configdialog/configdialog.rb
@@ -22,85 +22,88 @@
 
 ** Translated to QtRuby by Richard Dale
 =end
-	
+    
 require 'pages.rb'
 
 class ConfigDialog < Qt::Dialog
-	
-	slots 'changePage(QListWidgetItem*, QListWidgetItem*)'
-	
-	def initialize()
-		super
-	    @contentsWidget = Qt::ListWidget.new do |c|
-			c.viewMode = Qt::ListView::IconMode
-			c.iconSize = Qt::Size.new(96, 84)
-			c.movement = Qt::ListView::Static
-			c.maximumWidth = 128
-			c.spacing = 12
-		end
-	
-	    @pagesWidget = Qt::StackedWidget.new do |p|
-	    	p.addWidget(ConfigurationPage.new)
-	    	p.addWidget(UpdatePage.new)
-	    	p.addWidget(QueryPage.new)
-		end
-	
-	    closeButton = Qt::PushButton.new(tr("Close"))
-	
-	    createIcons()
-	    @contentsWidget.currentRow = 0
-	
-	    connect(closeButton, SIGNAL('clicked()'), self, SLOT('close()'))
-	
-	    horizontalLayout = Qt::HBoxLayout.new do |h|
-	    	h.addWidget(@contentsWidget)
-	    	h.addWidget(@pagesWidget, 1)
-		end
-	
-	    buttonsLayout = Qt::HBoxLayout.new do |b|
-	    	b.addStretch(1)
-	    	b.addWidget(closeButton)
-		end
-	
-	    self.layout = Qt::VBoxLayout.new do |m|
-			m.addLayout(horizontalLayout)
-			m.addStretch(1)
-			m.addSpacing(12)
-			m.addLayout(buttonsLayout)
-		end
-	
-	    setWindowTitle(tr("Config Dialog"))
-	end
-	
-	def createIcons()
-	    configButton = Qt::ListWidgetItem.new(@contentsWidget)
-	    configButton.icon = Qt::Icon.new("images/config.png")
-	    configButton.text = tr("Configuration")
-	    configButton.textAlignment = Qt::AlignHCenter
-	    configButton.flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled
-	
-	    updateButton = Qt::ListWidgetItem.new(@contentsWidget)
-	    updateButton.icon = Qt::Icon.new("images/update.png")
-	    updateButton.text = tr("Update")
-	    updateButton.textAlignment = Qt::AlignHCenter
-	    updateButton.flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled
-	
-	    queryButton = Qt::ListWidgetItem.new(@contentsWidget)
-	    queryButton.icon = Qt::Icon.new("images/query.png")
-	    queryButton.text = tr("Query")
-	    queryButton.textAlignment = Qt::AlignHCenter
-	    queryButton.flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled
-	
-	    connect(@contentsWidget,
-	            SIGNAL('currentItemChanged(QListWidgetItem*, QListWidgetItem*)'),
-	            self, SLOT('changePage(QListWidgetItem*, QListWidgetItem*)'))
-	end
-	
-	def changePage(current, previous)
-	    if current.nil?
-	        current = previous
-		end
+    
+    slots 'changePage(QListWidgetItem*, QListWidgetItem*)'
+    
+    def initialize()
+        super
+        @contentsWidget = Qt::ListWidget.new do |c|
+            c.viewMode = Qt::ListView::IconMode
+            c.iconSize = Qt::Size.new(96, 84)
+            c.movement = Qt::ListView::Static
+            c.maximumWidth = 128
+            c.spacing = 12
+        end
+    
+        @pagesWidget = Qt::StackedWidget.new do |p|
+            p.addWidget(ConfigurationPage.new)
+            p.addWidget(UpdatePage.new)
+            p.addWidget(QueryPage.new)
+        end
+    
+        closeButton = Qt::PushButton.new(tr("Close"))
+    
+        createIcons()
+        @contentsWidget.currentRow = 0
+    
+        connect(closeButton, SIGNAL('clicked()'), self, SLOT('close()'))
+    
+        horizontalLayout = Qt::HBoxLayout.new do |h|
+            h.addWidget(@contentsWidget)
+            h.addWidget(@pagesWidget, 1)
+        end
+    
+        buttonsLayout = Qt::HBoxLayout.new do |b|
+            b.addStretch(1)
+            b.addWidget(closeButton)
+        end
+    
+        self.layout = Qt::VBoxLayout.new do |m|
+            m.addLayout(horizontalLayout)
+            m.addStretch(1)
+            m.addSpacing(12)
+            m.addLayout(buttonsLayout)
+        end
+    
+        self.windowTitle = tr("Config Dialog")
+    end
+    
+    def createIcons
+        configButton = Qt::ListWidgetItem.new(@contentsWidget) do |c|
+            c.icon = Qt::Icon.new("images/config.png")
+            c.text = tr("Configuration")
+            c.textAlignment = Qt::AlignHCenter
+            c.flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled
+        end
 
-	    @pagesWidget.currentIndex = @contentsWidget.row(current)
-	end
+        updateButton = Qt::ListWidgetItem.new(@contentsWidget) do |u|
+            u.icon = Qt::Icon.new("images/update.png")
+            u.text = tr("Update")
+            u.textAlignment = Qt::AlignHCenter
+            u.flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled
+        end
+
+        queryButton = Qt::ListWidgetItem.new(@contentsWidget) do |q|
+            q.icon = Qt::Icon.new("images/query.png")
+            q.text = tr("Query")
+            q.textAlignment = Qt::AlignHCenter
+            q.flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled
+        end
+    
+        connect(@contentsWidget,
+                SIGNAL('currentItemChanged(QListWidgetItem*, QListWidgetItem*)'),
+                self, SLOT('changePage(QListWidgetItem*, QListWidgetItem*)'))
+    end
+    
+    def changePage(current, previous)
+        if current.nil?
+            current = previous
+        end
+
+        @pagesWidget.currentIndex = @contentsWidget.row(current)
+    end
 end
--- qtruby/rubylib/examples/dialogs/configdialog/pages.rb
+++ qtruby/rubylib/examples/dialogs/configdialog/pages.rb
@@ -24,117 +24,117 @@
 =end
 
 class ConfigurationPage < Qt::Widget
-	
-	def initialize(parent = nil)
-	    super(parent)
-	    configGroup = Qt::GroupBox.new(tr("Server configuration"))
-	
-	    serverLabel = Qt::Label.new(tr("Server:"))
-	    serverCombo = Qt::ComboBox.new do |c|
-			c.addItem(tr("Trolltech (Australia)"))
-			c.addItem(tr("Trolltech (Norway)"))
-			c.addItem(tr("Trolltech (People's Republic of China)"))
-			c.addItem(tr("Trolltech (USA)"))
-		end
-	
-	    serverLayout = Qt::HBoxLayout.new do |s|
-	    	s.addWidget(serverLabel)
-	    	s.addWidget(serverCombo)
-		end
-	
-	    configGroup.layout = Qt::VBoxLayout.new do |c|
-	    	c.addLayout(serverLayout)
-		end
-	
-	    self.layout = Qt::VBoxLayout.new do |m|
-	    	m.addWidget(configGroup)
-	    	m.addStretch(1)
-		end
-	end
+    
+    def initialize(parent = nil)
+        super(parent)
+        configGroup = Qt::GroupBox.new(tr("Server configuration"))
+    
+        serverLabel = Qt::Label.new(tr("Server:"))
+        serverCombo = Qt::ComboBox.new do |c|
+            c.addItem(tr("Trolltech (Australia)"))
+            c.addItem(tr("Trolltech (Norway)"))
+            c.addItem(tr("Trolltech (People's Republic of China)"))
+            c.addItem(tr("Trolltech (USA)"))
+        end
+    
+        serverLayout = Qt::HBoxLayout.new do |s|
+            s.addWidget(serverLabel)
+            s.addWidget(serverCombo)
+        end
+    
+        configGroup.layout = Qt::VBoxLayout.new do |c|
+            c.addLayout(serverLayout)
+        end
+    
+        self.layout = Qt::VBoxLayout.new do |m|
+            m.addWidget(configGroup)
+            m.addStretch(1)
+        end
+    end
 end
 
 class QueryPage < Qt::Widget
-	
-	def initialize(parent = nil)
-	    super(parent)
-	    updateGroup = Qt::GroupBox.new(tr("Package selection"))
-	    systemCheckBox = Qt::CheckBox.new(tr("Update system"))
-	    appsCheckBox = Qt::CheckBox.new(tr("Update applications"))
-	    docsCheckBox = Qt::CheckBox.new(tr("Update documentation"))
-	
-	    packageGroup = Qt::GroupBox.new(tr("Existing packages"))
-	
-	    packageList = Qt::ListWidget.new
-	    qtItem = Qt::ListWidgetItem.new(packageList)
-	    qtItem.text = tr("Qt")
-	    qsaItem = Qt::ListWidgetItem.new(packageList)
-	    qsaItem.text = tr("QSA")
-	    teamBuilderItem = Qt::ListWidgetItem.new(packageList)
-	    teamBuilderItem.text = tr("Teambuilder")
-	
-	    startUpdateButton = Qt::PushButton.new(tr("Start update"))
-	
-	    updateGroup.layout = Qt::VBoxLayout.new do |u|
-			u.addWidget(systemCheckBox)
-			u.addWidget(appsCheckBox)
-			u.addWidget(docsCheckBox)
-	    end
-	
-	    packageGroup.layout = Qt::VBoxLayout.new do |p|
-	    	p.addWidget(packageList)
-	    end
-	
-	    self.layout = Qt::VBoxLayout.new do |m|
-			m.addWidget(updateGroup)
-			m.addWidget(packageGroup)
-			m.addSpacing(12)
-			m.addWidget(startUpdateButton)
-			m.addStretch(1)
-	    end
-	end
+    
+    def initialize(parent = nil)
+        super(parent)
+        updateGroup = Qt::GroupBox.new(tr("Package selection"))
+        systemCheckBox = Qt::CheckBox.new(tr("Update system"))
+        appsCheckBox = Qt::CheckBox.new(tr("Update applications"))
+        docsCheckBox = Qt::CheckBox.new(tr("Update documentation"))
+    
+        packageGroup = Qt::GroupBox.new(tr("Existing packages"))
+    
+        packageList = Qt::ListWidget.new
+        qtItem = Qt::ListWidgetItem.new(packageList)
+        qtItem.text = tr("Qt")
+        qsaItem = Qt::ListWidgetItem.new(packageList)
+        qsaItem.text = tr("QSA")
+        teamBuilderItem = Qt::ListWidgetItem.new(packageList)
+        teamBuilderItem.text = tr("Teambuilder")
+    
+        startUpdateButton = Qt::PushButton.new(tr("Start update"))
+    
+        updateGroup.layout = Qt::VBoxLayout.new do |u|
+            u.addWidget(systemCheckBox)
+            u.addWidget(appsCheckBox)
+            u.addWidget(docsCheckBox)
+        end
+    
+        packageGroup.layout = Qt::VBoxLayout.new do |p|
+            p.addWidget(packageList)
+        end
+    
+        self.layout = Qt::VBoxLayout.new do |m|
+            m.addWidget(updateGroup)
+            m.addWidget(packageGroup)
+            m.addSpacing(12)
+            m.addWidget(startUpdateButton)
+            m.addStretch(1)
+        end
+    end
 end
 
 class UpdatePage < Qt::Widget
-	
-	def initialize(parent = nil)
-	    super(parent)
-	    packagesGroup = Qt::GroupBox.new(tr("Look for packages"))
-	
-	    nameLabel = Qt::Label.new(tr("Name:"))
-	    nameEdit = Qt::LineEdit.new
-	
-	    dateLabel = Qt::Label.new(tr("Released after:"))
-	    dateEdit = Qt::DateTimeEdit.new(Qt::Date.currentDate())
-	
-	    releasesCheckBox = Qt::CheckBox.new(tr("Releases"))
-	    upgradesCheckBox = Qt::CheckBox.new(tr("Upgrades"))
-	
-	    hitsSpinBox = Qt::SpinBox.new do |h|
-			h.prefix = tr("Return up to ")
-			h.suffix = tr(" results")
-			h.specialValueText = tr("Return only the first result")
-			h.minimum = 1
-			h.maximum = 100
-			h.singleStep = 10
-		end
+    
+    def initialize(parent = nil)
+        super(parent)
+        packagesGroup = Qt::GroupBox.new(tr("Look for packages"))
+    
+        nameLabel = Qt::Label.new(tr("Name:"))
+        nameEdit = Qt::LineEdit.new
+    
+        dateLabel = Qt::Label.new(tr("Released after:"))
+        dateEdit = Qt::DateTimeEdit.new(Qt::Date.currentDate())
+    
+        releasesCheckBox = Qt::CheckBox.new(tr("Releases"))
+        upgradesCheckBox = Qt::CheckBox.new(tr("Upgrades"))
+    
+        hitsSpinBox = Qt::SpinBox.new do |h|
+            h.prefix = tr("Return up to ")
+            h.suffix = tr(" results")
+            h.specialValueText = tr("Return only the first result")
+            h.minimum = 1
+            h.maximum = 100
+            h.singleStep = 10
+        end
 
-	    startQueryButton = Qt::PushButton.new(tr("Start query"))
-	
-	    packagesGroup.layout = Qt::GridLayout.new do |p|
-			p.addWidget(nameLabel, 0, 0)
-			p.addWidget(nameEdit, 0, 1)
-			p.addWidget(dateLabel, 1, 0)
-			p.addWidget(dateEdit, 1, 1)
-			p.addWidget(releasesCheckBox, 2, 0)
-			p.addWidget(upgradesCheckBox, 3, 0)
-			p.addWidget(hitsSpinBox, 4, 0, 1, 2)
-	    end
-	
-	    self.layout = Qt::VBoxLayout.new do |m|
-			m.addWidget(packagesGroup)
-			m.addSpacing(12)
-			m.addWidget(startQueryButton)
-			m.addStretch(1)
-	    end
-	end
+        startQueryButton = Qt::PushButton.new(tr("Start query"))
+    
+        packagesGroup.layout = Qt::GridLayout.new do |p|
+            p.addWidget(nameLabel, 0, 0)
+            p.addWidget(nameEdit, 0, 1)
+            p.addWidget(dateLabel, 1, 0)
+            p.addWidget(dateEdit, 1, 1)
+            p.addWidget(releasesCheckBox, 2, 0)
+            p.addWidget(upgradesCheckBox, 3, 0)
+            p.addWidget(hitsSpinBox, 4, 0, 1, 2)
+        end
+    
+        self.layout = Qt::VBoxLayout.new do |m|
+            m.addWidget(packagesGroup)
+            m.addSpacing(12)
+            m.addWidget(startQueryButton)
+            m.addStretch(1)
+        end
+    end
 end
