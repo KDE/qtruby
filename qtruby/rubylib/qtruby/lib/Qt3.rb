fd46df8f666c4a9940d200914fddf11f099644cc
Caleb Tennis <caleb@gentoo.org>
2006-10-31 15:21:31 +0000

    A small change to the Qt3 and Qt4 files to prevent someone from trying to load both libraries at the same time (which causes ruby to crash).
    CCMAIL: kde-bindings@kde.org
    
    
    svn path=/trunk/KDE/kdebindings/qtruby/; revision=600729

--- qtruby/rubylib/qtruby/lib/Qt3.rb
+++ qtruby/rubylib/qtruby/lib/Qt3.rb
@@ -1,1 +1,6 @@
-require 'qtruby'
+begin
+  version = Qt::version
+  raise LoadError.new("Qt4 already loaded") unless version =~ /^3/
+rescue NameError
+  require 'qtruby'
+end
--- qtruby/rubylib/qtruby/lib/Qt4.rb
+++ qtruby/rubylib/qtruby/lib/Qt4.rb
@@ -1,1 +1,6 @@
-require 'qtruby4'
+begin
+  version = Qt::version
+  raise LoadError.new("Qt3 already loaded") unless version =~ /^4/
+rescue NameError
+  require 'qtruby4'
+end
