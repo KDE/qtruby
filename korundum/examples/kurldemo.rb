27166fa8f60f2e516b3047d494e75a43b5073f59
Andreas Pakulat <apaku@gmx.de>
2008-03-30 02:17:33 +0000

    Fix the code for Korundum4, the only thing I couldn't find out how to do is adding the cmdline option. Ruby always complained about "add" being an undefined method on that object. CCMAIL:kde-bindings@kde.org
    svn path=/trunk/KDE/kdebindings/ruby/; revision=791649

--- korundum/examples/kurldemo.rb
+++ korundum/examples/kurldemo.rb
@@ -31,7 +31,7 @@ dealings in this Software without prior written authorization from the
 copyright holder.
 =end
 
-require 'Korundum'
+require 'korundum4'
 
 class MainWin < KDE::MainWindow
     def initialize(*args)
@@ -43,46 +43,46 @@ class MainWin < KDE::MainWindow
         edit = KDE::TextEdit.new(self)
         setCentralWidget(edit)
 
-        edit.insert("KURL Demo\n")
-        edit.insert("\nAdding these urls:\n\n")
+        edit.insertPlainText("KURL Demo\n")
+        edit.insertPlainText("\nAdding these urls:\n\n")
         @urls.each do |url|
-            edit.insert("    #{url}\n")
+            edit.insertPlainText("    #{url}\n")
 		end
 
-        edit.insert("\nCreating KURLs (iterating to print):\n\n")
+        edit.insertPlainText("\nCreating KURLs (iterating to print):\n\n")
         urlList = []
         @urls.each do |url|
-            urlList << KDE::URL.new(url)
+            urlList << KDE::Url.new(url)
 		end
 
         urlList.each do |url|
-            edit.insert("     #{url.url()}\n")
+            edit.insertPlainText("     #{url.url()}\n")
 		end
 
-        edit.insert("\nFirst url -- urlList [0]:\n\n")
-        edit.insert("    #{urlList[0].to_s}\n")
-        edit.insert("    #{urlList[0].url()}\n")
+        edit.insertPlainText("\nFirst url -- urlList [0]:\n\n")
+        edit.insertPlainText("    #{urlList[0].to_s}\n")
+        edit.insertPlainText("    #{urlList[0].url()}\n")
 
-        edit.insert("\nLast url -- urlList [-1]:\n\n")
-        edit.insert("    #{urlList[-1].to_s}\n")
-        edit.insert("    #{urlList[-1].url()}\n")
+        edit.insertPlainText("\nLast url -- urlList [-1]:\n\n")
+        edit.insertPlainText("    #{urlList[-1].to_s}\n")
+        edit.insertPlainText("    #{urlList[-1].url()}\n")
 
-        edit.insert("\nMiddle urls -- urlList [2,4]:\n\n")
+        edit.insertPlainText("\nMiddle urls -- urlList [2,4]:\n\n")
         ulist = urlList[2,4]
         ulist.each do |url|
-            edit.insert("    #{url.to_s}\n")
-            edit.insert("    #{url.url()}\n")
+            edit.insertPlainText("    #{url.to_s}\n")
+            edit.insertPlainText("    #{url.url()}\n")
 		end
 
-        edit.insert("\nLength of urlList -- len (urlList):\n\n")
-        edit.insert("    Length = #{urlList.length}\n")
+        edit.insertPlainText("\nLength of urlList -- len (urlList):\n\n")
+        edit.insertPlainText("    Length = #{urlList.length}\n")
 
-        edit.insert("\nurl in urlList? -- KURL (\"http://yahoo.com\") in urlList\n\n")
-        edit.insert("    KDE::URL.new(\"http://yahoo.com\") in urlList = ")
-		tmp = KDE::URL.new("http://yahoo.com")
+        edit.insertPlainText("\nurl in urlList? -- KURL (\"http://yahoo.com\") in urlList\n\n")
+        edit.insertPlainText("    KDE::URL.new(\"http://yahoo.com\") in urlList = ")
+		tmp = KDE::Url.new("http://yahoo.com")
 		urlList.each do |url| 
 			if url.url == tmp.url
-        		edit.insert("true")
+        		edit.insertPlainText("true")
 			end
 		end
 	end
@@ -93,18 +93,18 @@ end
 
 description = "A basic application template"
 version     = "1.0"
-aboutData   = KDE::AboutData.new("", "",
-    version, description, KDE::AboutData::License_GPL,
-    "(C) 2003 whoever the author is")
+aboutData   = KDE::AboutData.new("urldemo", "", KDE.ki18n("URL demo app"),
+    version, KDE.ki18n(description),KDE::AboutData::License_GPL,KDE.ki18n("(C) 2003 whoever the author is"))
 
-aboutData.addAuthor("author1", "whatever they did", "email@somedomain")
-aboutData.addAuthor("author2", "they did something else", "another@email.address")
+aboutData.addAuthor(KDE.ki18n("author1"), KDE.ki18n("whatever they did"), "email@somedomain")
+aboutData.addAuthor(KDE.ki18n("author2"), KDE.ki18n("they did something else"), "another@email.address")
 
 KDE::CmdLineArgs.init(ARGV, aboutData)
-
-KDE::CmdLineArgs.addCmdLineOptions([["+files", "File to open", ""]])
+cmdlineoptions = KDE::CmdLineOptions.new()
+#cmdlineoptions.add("+files", KDE.ki18n("File to open"))
+KDE::CmdLineArgs.addCmdLineOptions(cmdlineoptions)
 
 app = KDE::Application.new()
-mainWindow = MainWin.new(nil, "main window")
+mainWindow = MainWin.new(nil)
 mainWindow.show()
 app.exec()
