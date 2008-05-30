5680eff8634a651e00325ffa9d0911cb4b58211b
Richard Dale <richard.j.dale@gmail.com>
2008-05-30 13:08:52 +0000

    * Add a qwt ruby extension to go with the smoke lib
    CCMAIL: kde-bindings@kde.org
    
    svn path=/trunk/KDE/kdebindings/ruby/; revision=814491

---
+++ qwt/qwt.cpp
@@ -0,0 +1,60 @@
+#include <ruby.h>
+
+#include <QHash>
+#include <QList>
+#include <QtDebug>
+
+#include <qwt/qwt_smoke.h>
+
+#include <qtruby.h>
+
+#include <iostream>
+
+static VALUE getClassList(VALUE /*self*/)
+{
+    VALUE classList = rb_ary_new();
+    for (int i = 1; i < qwt_Smoke->numClasses; i++) {
+        if (qwt_Smoke->classes[i].className && !qwt_Smoke->classes[i].external)
+            rb_ary_push(classList, rb_str_new2(qwt_Smoke->classes[i].className));
+    }
+    return classList;
+}
+
+const char*
+resolve_classname_qwt(Smoke* smoke, int classId, void* /*ptr*/)
+{
+    return smoke->binding->className(classId);
+}
+
+extern TypeHandler Qwt_handlers[];
+
+extern "C" {
+
+VALUE qwt_module;
+VALUE qwt_internal_module;
+
+Q_DECL_EXPORT void
+Init_qwt()
+{
+    rb_require("Qt4");    // need to initialize the core runtime first
+    init_qwt_Smoke();
+
+    qwt_Smoke->binding = new QtRubySmokeBinding(qwt_Smoke);
+
+    smokeList << qwt_Smoke;
+
+    QtRubyModule module = { "Qwt", resolve_classname_qwt, 0 };
+    qtruby_modules[qwt_Smoke] = module;
+
+    install_handlers(Qwt_handlers);
+
+    qwt_module = rb_define_module("Qwt");
+    qwt_internal_module = rb_define_module_under(qwt_module, "Internal");
+
+    rb_define_singleton_method(qwt_internal_module, "getClassList", (VALUE (*) (...)) getClassList, 0);
+
+    rb_require("qwt/qwt.rb");
+    rb_funcall(qwt_internal_module, rb_intern("init_all_classes"), 0);
+}
+
+}
---
+++ qwt/qwt.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+require 'Qt4'
+
+module Qwt
+    module Internal
+        def self.init_all_classes
+            Qt::Internal::add_normalize_proc(Proc.new do |classname|
+                if classname =~ /^Qsci/
+                    now = classname.sub(/^Qwt?(?=[A-Z])/,'Qwt::')
+                end
+                now
+            end)
+            getClassList.each do |c|
+                classname = Qt::Internal::normalize_classname(c)
+                id = Qt::Internal::findClass(c);
+                Qt::Internal::insert_pclassid(classname, id)
+                Qt::Internal::cpp_names[classname] = c
+                klass = Qt::Internal::isQObject(c) ? Qt::Internal::create_qobject_class(classname, Qwt) \
+                                                    : Qt::Internal::create_qt_class(classname, Qwt)
+                Qt::Internal::classes[classname] = klass unless klass.nil?
+            end
+        end
+    end
+end
