b3041ad42d5d2ba2b12a0dc5d55da634d6652db1
Arno Rehn <arno@arnorehn.de>
2008-05-31 10:43:29 +0000

    * Added qscintilla classes and headers from the 2.2 release * Updated the C# qscintilla classes * Added classlist support for the kimono target * Added support for --smoke-invocation in kalyptus for easier   generation of additional classes * Updated the AssemblyInfo.cs files * Fixed qtruby.cpp's header * Don't embed qtruby in korundum, link against qtruby4shared instead
    CCMAIL: kde-bindings@kde.org
    
    
    
    svn path=/trunk/KDE/kdebindings/ruby/; revision=814808

--- qtruby/src/qtruby.cpp
+++ qtruby/src/qtruby.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                          qtruby.cpp  -  description
+                             -------------------
+    begin                : Fri Jul 4 2003
+    copyright            : (C) 2003-2006 by Richard Dale
+    email                : Richard_Dale@tipitina.demon.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <QtCore/qabstractitemmodel.h>			
 #include <QtCore/qglobal.h>
 #include <QtCore/qhash.h>
