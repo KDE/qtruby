e94f030eb01adf4a1d3dda3840e2c45a6b7acfa0
Richard Dale <richard.j.dale@gmail.com>
2008-06-05 12:53:39 +0000

    * Add a ruby nepomuk module
    CCMAIL: kde-bindings@kde.org
    
    svn path=/trunk/KDE/kdebindings/ruby/; revision=817171

---
+++ nepomuk/nepomuk.cpp
@@ -0,0 +1,76 @@
+/***************************************************************************
+                          nepomuk.cpp  -  Nepomuk ruby extension
+                             -------------------
+    begin                : Thurs May 29 2008
+    copyright            : (C) 2008 by Richard Dale
+    email                : Richard_Dale@tipitina.demon.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <ruby.h>
+
+#include <QHash>
+#include <QList>
+#include <QtDebug>
+
+#include <smoke/nepomuk_smoke.h>
+
+#include <qtruby.h>
+
+#include <iostream>
+
+static VALUE getClassList(VALUE /*self*/)
+{
+    VALUE classList = rb_ary_new();
+    for (int i = 1; i < nepomuk_Smoke->numClasses; i++) {
+        if (nepomuk_Smoke->classes[i].className && !nepomuk_Smoke->classes[i].external)
+            rb_ary_push(classList, rb_str_new2(nepomuk_Smoke->classes[i].className));
+    }
+    return classList;
+}
+
+const char*
+resolve_classname_nepomuk(Smoke* smoke, int classId, void* /*ptr*/)
+{
+    return smoke->binding->className(classId);
+}
+
+extern TypeHandler Nepomuk_handlers[];
+
+extern "C" {
+
+VALUE nepomuk_module;
+VALUE nepomuk_internal_module;
+
+Q_DECL_EXPORT void
+Init_nepomuk()
+{
+    init_nepomuk_Smoke();
+
+    nepomuk_Smoke->binding = new QtRubySmokeBinding(nepomuk_Smoke);
+
+    smokeList << nepomuk_Smoke;
+
+    QtRubyModule module = { "Nepomuk", resolve_classname_nepomuk, 0 };
+    qtruby_modules[nepomuk_Smoke] = module;
+
+    install_handlers(Nepomuk_handlers);
+
+    nepomuk_module = rb_define_module("Nepomuk");
+    nepomuk_internal_module = rb_define_module_under(nepomuk_module, "Internal");
+
+    rb_define_singleton_method(nepomuk_internal_module, "getClassList", (VALUE (*) (...)) getClassList, 0);
+
+    rb_require("nepomuk/nepomuk.rb");
+    rb_funcall(nepomuk_internal_module, rb_intern("init_all_classes"), 0);
+}
+
+}
---
+++ nepomuk/nepomukhandlers.cpp
@@ -0,0 +1,229 @@
+/***************************************************************************
+                          nepomukhandlers.cpp  -  Nepomuk specific marshallers
+                             -------------------
+    begin                : Thurs May 29 2008
+    copyright            : (C) 2008 by Richard Dale
+    email                : Richard_Dale@tipitina.demon.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <ruby.h>
+
+#include <qtruby.h>
+#include <smokeruby.h>
+
+#include <nepomuk/resource.h>
+#include <nepomuk/tag.h>
+
+template <class Item, class ItemList, const char *ItemSTR >
+void marshall_ItemList(Marshall *m) {
+	switch(m->action()) {
+		case Marshall::FromVALUE:
+		{
+			VALUE list = *(m->var());
+			if (TYPE(list) != T_ARRAY) {
+				m->item().s_voidp = 0;
+				break;
+			}
+
+			int count = RARRAY(list)->len;
+			ItemList *cpplist = new ItemList;
+			long i;
+			for(i = 0; i < count; i++) {
+				VALUE item = rb_ary_entry(list, i);
+				// TODO do type checking!
+				smokeruby_object *o = value_obj_info(item);
+				if(!o || !o->ptr)
+					continue;
+				void *ptr = o->ptr;
+				ptr = o->smoke->cast(
+					ptr,				// pointer
+					o->classId,				// from
+		    		o->smoke->idClass(ItemSTR).index	// to
+				);
+				cpplist->append((Item*)ptr);
+			}
+
+			m->item().s_voidp = cpplist;
+			m->next();
+
+			if (!m->type().isConst()) {
+				rb_ary_clear(list);
+	
+				for(int i = 0; i < cpplist->size(); ++i ) {
+					VALUE obj = getPointerObject( (void*) cpplist->at(i) );
+					rb_ary_push(list, obj);
+				}
+			}
+
+			if (m->cleanup()) {
+				delete cpplist;
+			}
+		}
+		break;
+      
+		case Marshall::ToVALUE:
+		{
+			ItemList *valuelist = (ItemList*)m->item().s_voidp;
+			if(!valuelist) {
+				*(m->var()) = Qnil;
+				break;
+			}
+
+			VALUE av = rb_ary_new();
+			int ix = m->smoke()->idClass(ItemSTR).index;
+			const char * className = m->smoke()->binding->className(ix);
+
+			for (int i=0; i<valuelist->size(); ++i) {
+				void *p = (void*) valuelist->at(i);
+
+				if (m->item().s_voidp == 0) {
+					*(m->var()) = Qnil;
+					break;
+				}
+
+				VALUE obj = getPointerObject(p);
+				if (obj == Qnil) {
+					smokeruby_object  * o = alloc_smokeruby_object(false, m->smoke(), ix, p);
+					obj = set_obj_info(className, o);
+				}
+			
+				rb_ary_push(av, obj);
+			}
+
+			*(m->var()) = av;
+			m->next();
+
+			if (m->cleanup()) {
+				delete valuelist;
+			}
+		}
+		break;
+
+		default:
+			m->unsupported();
+		break;
+   }
+}
+
+#define DEF_LIST_MARSHALLER(ListIdent,ItemList,Item) namespace { char ListIdent##STR[] = #Item; }  \
+        Marshall::HandlerFn marshall_##ListIdent = marshall_ItemList<Item,ItemList,ListIdent##STR>;
+
+
+template <class Item, class ItemList, const char *ItemSTR >
+void marshall_ValueListItem(Marshall *m) {
+	switch(m->action()) {
+		case Marshall::FromVALUE:
+		{
+			VALUE list = *(m->var());
+			if (TYPE(list) != T_ARRAY) {
+				m->item().s_voidp = 0;
+				break;
+			}
+			int count = RARRAY(list)->len;
+			ItemList *cpplist = new ItemList;
+			long i;
+			for(i = 0; i < count; i++) {
+				VALUE item = rb_ary_entry(list, i);
+				// TODO do type checking!
+				smokeruby_object *o = value_obj_info(item);
+
+				if (!o || !o->ptr)
+					continue;
+				
+				void *ptr = o->ptr;
+				ptr = o->smoke->cast(
+					ptr,				// pointer
+					o->classId,				// from
+					o->smoke->idClass(ItemSTR).index	        // to
+				);
+				cpplist->append(*(Item*)ptr);
+			}
+
+			m->item().s_voidp = cpplist;
+			m->next();
+
+			if (!m->type().isConst()) {
+				rb_ary_clear(list);
+				for(int i=0; i < cpplist->size(); ++i) {
+					VALUE obj = getPointerObject((void*)&(cpplist->at(i)));
+					rb_ary_push(list, obj);
+				}
+			}
+
+			if (m->cleanup()) {
+				delete cpplist;
+			}
+		}
+		break;
+      
+		case Marshall::ToVALUE:
+		{
+			ItemList *valuelist = (ItemList*)m->item().s_voidp;
+			if(!valuelist) {
+				*(m->var()) = Qnil;
+				break;
+			}
+
+			VALUE av = rb_ary_new();
+
+			int ix = m->smoke()->idClass(ItemSTR).index;
+			const char * className = m->smoke()->binding->className(ix);
+
+			for(int i=0; i < valuelist->size() ; ++i) {
+				void *p = (void *) &(valuelist->at(i));
+
+				if(m->item().s_voidp == 0) {
+				*(m->var()) = Qnil;
+				break;
+				}
+
+				VALUE obj = getPointerObject(p);
+				if(obj == Qnil) {
+					smokeruby_object  * o = alloc_smokeruby_object(	false, 
+																	m->smoke(), 
+																	m->smoke()->idClass(ItemSTR).index, 
+																	p );
+					obj = set_obj_info(className, o);
+				}
+		
+				rb_ary_push(av, obj);
+			}
+
+			*(m->var()) = av;
+			m->next();
+
+			if (m->cleanup()) {
+				delete valuelist;
+			}
+
+		}
+		break;
+      
+		default:
+			m->unsupported();
+		break;
+	}
+}
+
+#define DEF_VALUELIST_MARSHALLER(ListIdent,ItemList,Item) namespace { char ListIdent##STR[] = #Item; }  \
+        Marshall::HandlerFn marshall_##ListIdent = marshall_ValueListItem<Item,ItemList,ListIdent##STR>;
+
+DEF_VALUELIST_MARSHALLER( NepomukResourceList, QList<Nepomuk::Resource>, Nepomuk::Resource )
+DEF_VALUELIST_MARSHALLER( NepomukTagList, QList<Nepomuk::Tag>, Nepomuk::Tag )
+
+TypeHandler Nepomuk_handlers[] = {
+    { "QList<Nepomuk::Resource>", marshall_NepomukResourceList },
+    { "QList<Nepomuk::Resource>&", marshall_NepomukResourceList },
+    { "QList<Nepomuk::Tag>", marshall_NepomukTagList },
+    { "QList<Nepomuk::Tag>&", marshall_NepomukTagList },
+    { 0, 0 }
+};
