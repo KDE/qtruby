5f52835e6fdb7fff26467352e8e0cd964f84282d
Richard Dale <richard.j.dale@gmail.com>
2008-06-08 19:15:52 +0000

    * Move the template based List marshallers to a new header   marshall.macros.h and add marshallers for QLinkedList types * Add some marshallers to the kdevplatform and okular extensions
    svn path=/trunk/KDE/kdebindings/ruby/; revision=818550

--- kdevplatform/kdevplatformhandlers.cpp
+++ kdevplatform/kdevplatformhandlers.cpp
@@ -19,61 +19,97 @@
 
 #include <qtruby.h>
 #include <smokeruby.h>
-#include <marshall_basetypes.h>
+#include <marshall_macros.h>
 
-/*
+#include <kdevplatform/editor/documentrange.h>
+#include <kdevplatform/interfaces/idocument.h>
+#include <kdevplatform/interfaces/ilanguage.h>
+#include <kdevplatform/interfaces/iplugin.h>
+#include <kdevplatform/interfaces/iproject.h>
+#include <kdevplatform/language/interfaces/iproblem.h>
+#include <kdevplatform/language/duchain/declaration.h>
+#include <kdevplatform/language/duchain/ducontext.h>
+#include <kdevplatform/language/duchain/identifier.h>
+#include <kdevplatform/language/duchain/topducontext.h>
+#include <kdevplatform/language/interfaces/quickopendataprovider.h>
+#include <kdevplatform/project/projectmodel.h>
+#include <kdevplatform/shell/language.h>
+#include <kdevplatform/shell/profile.h>
+#include <kdevplatform/sublime/area.h>
+#include <kdevplatform/sublime/document.h>
+#include <kdevplatform/sublime/mainwindow.h>
+#include <kdevplatform/sublime/tooldocument.h>
+#include <kdevplatform/sublime/urldocument.h>
+#include <kdevplatform/sublime/view.h>
+
+/* TODO
 "KDevelop::DUContext::DeclarationList&",
 "KDevelop::DUContext::SearchItem::PtrList",
 "KDevelop::DUContext::SearchItem::PtrList&",
-"KDevelop::EnvironmentGroupList",
-"KDevelop::EnvironmentGroupList*",
-"KDevelop::EnvironmentGroupList&",
-"KDevelop::Profile::EntryList",
-"KDevelop::Profile::EntryList&",
-"KDevelop::Profile::List",
-"KDevelop::ProfileListing*",
-"KDevelop::ProfileListing&",
-"KDevelop::ProfileListingEx*",
-"KDevelop::ProfileListingEx&",
-"KPluginInfo::List",
 "QList<KDevelop::AbstractType::Ptr>&",
-"QList<KDevelop::Declaration*>",
-"QList<KDevelop::DocumentRange*>&",
-"QList<KDevelop::DUContext*>",
-"QList<KDevelop::DUContext*>&",
-"QList<KDevelop::IDocument*>",
-"QList<KDevelop::ILanguage*>",
-"QList<KDevelop::IPlugin*>",
-"QList<KDevelop::IProject*>",
-"QList<KDevelop::ProblemPointer>",
-"QList<KDevelop::Profile*>",
-"QList<KDevelop::ProjectBaseItem*>",
-"QList<KDevelop::ProjectFileItem*>",
-"QList<KDevelop::ProjectFolderItem*>",
-"QList<KDevelop::ProjectTargetItem*>",
-"QList<KDevelop::QuickOpenDataPointer>",
-"QList<KDevelop::TopDUContext*>",
-"QList<KDevelop::TopDUContext*>&",
-"QList<KDevelop::TypeIdentifier>&",
-"QList<KParts::Part*>",
-"QList<KTextEditor::Document*>&",
-"QList<KTextEditor::SmartRange*>",
-"QList<KTextEditor::View*>&",
-"QList<KUrl>",
-"QList<QAction*>",
-"QList<QPair<KDevelop::Declaration*,int>
-"QList<QPair<KDevelop::TopDUContext*,KDevelop::SimpleCursor>
-"QList<QString>&",
-"QList<QVariant>",
-"QList<Sublime::Area*>&",
-"QList<Sublime::Document*>&",
-"QList<Sublime::MainWindow*>&",
-"QList<Sublime::View*>",
-"QList<Sublime::View*>&",
 "QMap<KDevelop::HashedString,QList<KDevelop::SimpleRange>
 */
 
-TypeHandler KDevPlatform_handlers[] = {
-    { 0, 0 }
+DEF_VALUELIST_MARSHALLER( KDevelopProblemPointerList, QList<KDevelop::ProblemPointer>, KDevelop::ProblemPointer )
+DEF_VALUELIST_MARSHALLER( KDevelopQuickOpenDataPointerList, QList<KDevelop::QuickOpenDataPointer>, KDevelop::QuickOpenDataPointer )
+DEF_VALUELIST_MARSHALLER( KDevelopTypeIdentifierList, QList<KDevelop::TypeIdentifier>, KDevelop::TypeIdentifier )
+DEF_VALUELIST_MARSHALLER( KDevelopProfileEntryList, QList<KDevelop::Profile::Entry>, KDevelop::Profile::Entry )
 
+DEF_LIST_MARSHALLER( KDevelopDUContextList, QList<KDevelop::DUContext*>, KDevelop::DUContext )
+DEF_LIST_MARSHALLER( KDevelopDeclarationList, QList<KDevelop::Declaration*>, KDevelop::Declaration )
+DEF_LIST_MARSHALLER( KDevelopDocumentRangeList, QList<KDevelop::DocumentRange*>, KDevelop::DocumentRange )
+DEF_LIST_MARSHALLER( KDevelopIDocumentList, QList<KDevelop::IDocument*>, KDevelop::IDocument )
+DEF_LIST_MARSHALLER( KDevelopILanguageList, QList<KDevelop::ILanguage*>, KDevelop::ILanguage )
+DEF_LIST_MARSHALLER( KDevelopIPluginList, QList<KDevelop::IPlugin*>, KDevelop::IPlugin )
+DEF_LIST_MARSHALLER( KDevelopIProjectList, QList<KDevelop::IProject*>, KDevelop::IProject )
+DEF_LIST_MARSHALLER( KDevelopProfileList, QList<KDevelop::Profile*>, KDevelop::Profile )
+DEF_LIST_MARSHALLER( KDevelopProjectBaseItemList, QList<KDevelop::ProjectBaseItem*>, KDevelop::ProjectBaseItem )
+DEF_LIST_MARSHALLER( KDevelopProjectFileItemList, QList<KDevelop::ProjectFileItem*>, KDevelop::ProjectFileItem )
+DEF_LIST_MARSHALLER( KDevelopProjectFolderItemList, QList<KDevelop::ProjectFolderItem*>, KDevelop::ProjectFolderItem )
+DEF_LIST_MARSHALLER( KDevelopProjectTargetItemList, QList<KDevelop::ProjectTargetItem*>, KDevelop::ProjectTargetItem )
+DEF_LIST_MARSHALLER( KDevelopTopDUContextList, QList<KDevelop::TopDUContext*>, KDevelop::TopDUContext )
+DEF_LIST_MARSHALLER( KPartsPartList, QList<KParts::Part*>, KParts::Part )
+DEF_LIST_MARSHALLER( KTextEditorDocumentList, QList<KTextEditor::Document*>, KTextEditor::Document )
+DEF_LIST_MARSHALLER( KTextEditorSmartRangeList, QList<KTextEditor::SmartRange*>, KTextEditor::SmartRange )
+DEF_LIST_MARSHALLER( KTextEditorViewList, QList<KTextEditor::View*>, KTextEditor::View )
+DEF_LIST_MARSHALLER( SublimeAreaList, QList<Sublime::Area*>, Sublime::Area )
+DEF_LIST_MARSHALLER( SublimeDocumentList, QList<Sublime::Document*>, Sublime::Document )
+DEF_LIST_MARSHALLER( SublimeMainWindowList, QList<Sublime::MainWindow*>, Sublime::MainWindow )
+DEF_LIST_MARSHALLER( SublimeViewList, QList<Sublime::View*>, Sublime::View )
+DEF_LIST_MARSHALLER( KDevelopDUContextVector, QVector<KDevelop::DUContext*>, KDevelop::DUContext )
+DEF_LIST_MARSHALLER( KDevelopDeclarationVector, QVector<KDevelop::Declaration*>, KDevelop::Declaration )
+
+TypeHandler KDevPlatform_handlers[] = {
+    { "QList<KDevelop::DUContext*>", marshall_KDevelopDUContextList },
+    { "QList<KDevelop::DUContext*>&", marshall_KDevelopDUContextList },
+    { "QList<KDevelop::Declaration*>", marshall_KDevelopDeclarationList },
+    { "QList<KDevelop::DocumentRange*>&", marshall_KDevelopDocumentRangeList },
+    { "QList<KDevelop::IDocument*>", marshall_KDevelopIDocumentList },
+    { "QList<KDevelop::ILanguage*>", marshall_KDevelopILanguageList },
+    { "QList<KDevelop::IPlugin*>", marshall_KDevelopIPluginList },
+    { "QList<KDevelop::IProject*>", marshall_KDevelopIProjectList },
+    { "QList<KDevelop::ProblemPointer>", marshall_KDevelopProblemPointerList },
+    { "QList<KDevelop::Profile*>", marshall_KDevelopProfileList },
+    { "KDevelop::Profile::EntryList", marshall_KDevelopProfileEntryList },
+    { "KDevelop::Profile::EntryList&", marshall_KDevelopProfileEntryList },
+    { "QList<KDevelop::ProjectBaseItem*>", marshall_KDevelopProjectBaseItemList },
+    { "QList<KDevelop::ProjectFileItem*>", marshall_KDevelopProjectFileItemList },
+    { "QList<KDevelop::ProjectFolderItem*>", marshall_KDevelopProjectFolderItemList },
+    { "QList<KDevelop::ProjectTargetItem*>", marshall_KDevelopProjectTargetItemList },
+    { "QList<KDevelop::QuickOpenDataPointer>", marshall_KDevelopQuickOpenDataPointerList },
+    { "QList<KDevelop::TopDUContext*>", marshall_KDevelopTopDUContextList },
+    { "QList<KDevelop::TopDUContext*>&", marshall_KDevelopTopDUContextList },
+    { "QList<KDevelop::TypeIdentifier>&", marshall_KDevelopTypeIdentifierList },
+    { "QList<KParts::Part*>", marshall_KPartsPartList },
+    { "QList<KTextEditor::Document*>&", marshall_KTextEditorDocumentList },
+    { "QList<KTextEditor::SmartRange*>", marshall_KTextEditorSmartRangeList },
+    { "QList<KTextEditor::View*>&", marshall_KTextEditorViewList },
+    { "QList<Sublime::Area*>&", marshall_SublimeAreaList },
+    { "QList<Sublime::Document*>&", marshall_SublimeDocumentList },
+    { "QList<Sublime::MainWindow*>&", marshall_SublimeMainWindowList },
+    { "QList<Sublime::View*>", marshall_SublimeViewList },
+    { "QList<Sublime::View*>&", marshall_SublimeViewList },
+    { "QVector<KDevelop::DUContext*>&", marshall_KDevelopDUContextVector },
+    { "QVector<KDevelop::Declaration*>", marshall_KDevelopDeclarationVector },
+    { 0, 0 }
 };
