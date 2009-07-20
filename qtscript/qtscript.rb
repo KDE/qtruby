a80ced49feef82cf6f1669e69cf948dc330be6fb
Richard Dale <richard.j.dale@gmail.com>
2009-07-20 22:18:11 +0000

    * Some of the QtScript classes, such as QScriptValue, were being wrongly put into a Ruby QScript:: module and named QScript::Value instead of Qt::ScriptValue. Thanks to David Palacio for reporting the bug
    svn path=/branches/KDE/4.3/kdebindings/ruby/; revision=1000146

--- qtscript/qtscript.rb
+++ qtscript/qtscript.rb
@@ -31,7 +31,8 @@ module QtScript
         id = Qt::Internal::findClass(c);
         Qt::Internal::insert_pclassid(classname, id)
         Qt::Internal::cpp_names[classname] = c
-        klass = Qt::Internal::isQObject(c) ? Qt::Internal::create_qobject_class(classname, Qt)                                            : Qt::Internal::create_qt_class(classname, QtScript)
+        klass = Qt::Internal::isQObject(c) ? Qt::Internal::create_qobject_class(classname, Qt) \
+                                           : Qt::Internal::create_qt_class(classname, Qt)
         Qt::Internal::classes[classname] = klass unless klass.nil?
       end
     end
