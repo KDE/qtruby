fe1bd0363984ce730fc66282c7622a9e34fa3204
Richard Dale <richard.j.dale@gmail.com>
2009-11-13 19:10:21 +0000

    * Add some 'macro-like' methods to Qt::Test to make it work like the C++ version. For instance, QVERIFY can be used like this:
        def myFirstTest()
              QVERIFY('1 == 1')
              qDebug("myFirstTest()")
        end
    
    CCMAIL: kde-bindings@kde.org
    CCMAIL: pilz@osp-dd.de
    
    
    svn path=/trunk/KDE/kdebindings/ruby/; revision=1048695

--- qttest/qttest.rb
+++ qttest/qttest.rb
@@ -31,3 +31,28 @@ module QtTest
     end
   end
 end
+
+module Qt
+  class Base
+    def QVERIFY(statement)
+      Qt::Test.qVerify(eval(statement), statement, "", __FILE__, __LINE__)
+    end
+    
+    def QFAIL(message)
+      Qt::Test.qFail(message, __FILE__, __LINE__)
+    end
+    
+    def QVERIFY2(statement, description)
+      Qt::Test.qVerify(eval(statement), statement, description, __FILE__, __LINE__)
+    end
+    
+    def QCOMPARE(actual, expected)
+      Qt::Test.qCompare(eval(actual), eval(expected), actual, expected, __FILE__, __LINE__)    
+    end
+    
+    def QSKIP(statement, mode)
+      Qt::Test.qSkip(statement, mode, __FILE__, __LINE__)
+    end
+  end
+end
+
