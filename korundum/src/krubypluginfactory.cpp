909782daf3fd0fd699410ce784bbaef70cd6af00
Arno Rehn <arno@arnorehn.de>
2010-07-07 10:38:30 +0000

    add kio functionality
    svn path=/trunk/KDE/kdebindings/ruby/; revision=1147062

--- korundum/src/krubypluginfactory.cpp
+++ korundum/src/krubypluginfactory.cpp
@@ -32,6 +32,8 @@
 #include <klibloader.h>
 #include <kdebug.h>
 
+#include <kio/slavebase.h>
+
 #include <qtruby.h>
 
 extern "C" {
@@ -92,7 +94,7 @@ class KRubyPluginFactory : public KPluginFactory
     protected:
         virtual QObject *create(const char *iface, QWidget *parentWidget, QObject *parent, const QVariantList &args, const QString &keyword);
 
-    private:
+    public:
         static QByteArray camelize(QByteArray name);
 };
 K_EXPORT_PLUGIN(KRubyPluginFactory)
@@ -261,4 +263,140 @@ QObject *KRubyPluginFactory::create(const char *iface, QWidget *parentWidget, QO
     if(createdInstance->parent() == 0)
         createdInstance->setParent(parent);
     return createdInstance;
+}
+
+VALUE slave_class = Qnil;
+
+static VALUE create_slave_instance(VALUE av)
+{
+    VALUE a = rb_ary_pop(av);
+    VALUE b = rb_ary_pop(av);
+    VALUE c = rb_ary_pop(av);
+
+    return rb_funcall(slave_class, rb_intern("new"), 3, c, b, a);
+}
+
+extern "C" {
+
+Q_DECL_EXPORT
+int kdemain(int argc, char **argv)
+{
+    if (argc != 4) {
+        printf("USAGE: krubypluginfactory protocol pool_sock app_sock");
+        return -1;
+    }
+
+    KComponentData("krubypluginfactory");
+
+    KRubyPluginFactory factory;
+
+    QByteArray protocol(argv[1]);
+    QByteArray pool_sock(argv[2]);
+    QByteArray app_sock(argv[3]);
+
+    // find the script
+    QString keyword("kio_");
+    keyword.append(protocol);
+    keyword.append("/main.rb");
+    QString path = KStandardDirs::locate("data", keyword);
+
+    if (path.isEmpty()) {
+        kWarning() << "Couldn't find" << keyword;
+        return -1;
+    }
+
+    QFileInfo program(path);
+
+#ifdef RUBY_INIT_STACK
+    RUBY_INIT_STACK
+#endif
+
+#if RUBY_VERSION < 0x10900
+    bool firstTime = (rb_load_path == 0);
+#else
+    bool firstTime = true;
+#endif
+
+    ruby_init();
+    ruby_script(QFile::encodeName(program.fileName()));
+
+    // If ruby_init_loadpath() is called more than once, it keeps
+    // adding the same standard directories to it.
+    if (firstTime) {
+        ruby_init_loadpath();
+    }
+
+    ruby_incpush(QFile::encodeName(program.path()));
+
+    int state = 0;
+    const QByteArray encodedFilePath = QFile::encodeName(program.filePath());
+    rb_load_protect(rb_str_new2(encodedFilePath), 0, &state);
+    if (state != 0) {
+        show_exception_message();
+        kWarning() << "Failed to load " << encodedFilePath << keyword << path;
+        return -1;
+    }
+
+    QByteArray module_name = KRubyPluginFactory::camelize(protocol);
+    VALUE slave_module = rb_const_get(rb_cObject, rb_intern(module_name));
+    if (slave_module == Qnil) {
+        kWarning() << "Constant " << module_name << " not found.";
+        return -1;
+    }
+
+    slave_class = rb_const_get(slave_module, rb_intern("Main"));
+    if (slave_class == Qnil) {
+        kWarning() << "Constant Main in module " << module_name << " not found.";
+        return -1;
+    }
+
+#define WRAP_BYTEARRAY(cvar, rvar) \
+    { \
+        smokeruby_object *o = alloc_smokeruby_object(true, byteArrayIndex.smoke, byteArrayIndex.index, cvar); \
+        const char *class_name = resolve_classname(o); \
+        rb_ary_push(rvar, set_obj_info(class_name, o)); \
+    }
+
+    VALUE av = rb_ary_new();
+    Smoke::ModuleIndex byteArrayIndex = Smoke::findClass("QByteArray");
+    WRAP_BYTEARRAY(new QByteArray(protocol), av)
+    WRAP_BYTEARRAY(new QByteArray(pool_sock), av)
+    WRAP_BYTEARRAY(new QByteArray(app_sock), av)
+
+#undef WRAP_BYTEARRAY
+
+    VALUE slave_value = rb_protect(create_slave_instance, av, &state);
+    if (state != 0) {
+        show_exception_message();
+        kWarning() << "Failed to create instance of slave instance.";
+        return -1;
+    }
+
+    // Set a global variable '$my_app_foo_bar + <numeric id>' to the value of the new
+    // instance of MyApp::FooBar to prevent it being GC'd. Note that it would be
+    // better to be able to come up with a way to discover all the plugin instances,
+    // and call rb_gc_mark() on them, in the mark phase of GC.
+    QByteArray variableBaseName("$");
+    variableBaseName += protocol;
+
+    // Handle multiple instances of the same class, and look for an unused global
+    // variable
+    QByteArray variableName;
+    VALUE variable = Qnil;
+    int id = 0;
+    do {
+        id++;
+        variableName = variableBaseName + QByteArray::number(id);
+        variable = rb_gv_get(variableName);
+    } while (variable != Qnil);
+    rb_gv_set(variableName, slave_value);
+
+    smokeruby_object *o = 0;
+    Data_Get_Struct(slave_value, smokeruby_object, o);
+    KIO::SlaveBase *slave = reinterpret_cast<KIO::SlaveBase *>(o->ptr);
+    slave->dispatchLoop();
+    delete slave;
+    return 0;
+}
+
 }
