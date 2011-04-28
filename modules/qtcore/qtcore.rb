bdf6acfacfd9dec8b91f7961abe3cebf2651304a
Richard Dale <richard.dale@codethink.co.uk>
2011-04-28 10:19:43 +0100

    * Make qtruby build

--- modules/qtcore/qtcore.rb
+++ modules/qtcore/qtcore.rb
@@ -1,3 +1,22 @@
+=begin
+     Copyright 2009-2011 by Richard Dale <richard.j.dale@gmail.com>
+
+     This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU Library General Public License as
+     published by the Free Software Foundation; either version 2, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details
+
+     You should have received a copy of the GNU Library General Public
+     License along with this program; if not, write to the
+     Free Software Foundation, Inc.,
+     51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+=end
+
 module Qt
   module DebugLevel
     Off, Minimal, High, Extensive = 0, 1, 2, 3
@@ -29,7 +48,7 @@ module Qt
     # Runs the initializer as far as allocating the Qt C++ instance.
     # Then use a throw to jump back to here with the C++ instance
     # wrapped in a new ruby variable of type T_DATA
-    def Internal.try_initialize(instance, *args)
+    def Internal.try_initialize(instance,  args)
       initializer = instance.method(:initialize)
       catch :newqt do
         initializer.call(*args)
